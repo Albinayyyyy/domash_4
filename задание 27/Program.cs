203569441cb3f2aae1be69bd6130abaadf7f0e22
Albina <albinasannikova71@gmail.com>
2022-10-16 00:07:21 +0300

    final

---
+++ задание 27/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void Sum(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum = sum + (n % 10);
+        n = n / 10;
+    } 
+    return sum;
+} 
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.Write($"Сумма цифр числа {N} равна {(N)}.");
---
+++ задание25/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+
+
+Console.Clear();
+Console.Write("Вводим первое числа: ");
+int a =int.Parse(Console.ReadLine()!);
+Console.Write("Вводим  второе числа: ");
+int b =int.Parse(Console.ReadLine()!);
+Console.WriteLine(Pow(a, b));
+int Pow(int A, int B)
+{
+    int pow = 1;
+    int i = 0;
+    while (i < B)
+    {
+      pow = pow * A;
+      i = i + 1;
+    }
+    return pow;
+}
+
+
